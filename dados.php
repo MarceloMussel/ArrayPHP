c014126cf9c0b43517f69fbb918e931be1d9d7cf
MarceloMussel <marcelo.mussel@gmail.com>
2024-02-27 17:30:48 -0300

    Exemplo array

---
+++ dados.php
@@ -0,0 +1,8 @@
+<?php
+    $dados[] = array("nome" => "Marcelo", "idade" => 53, "estilo" => "Rock");
+    $dados[] = array("nome" => "Joseph Kliner", "idade" => 85, "estilo" => "Classica");
+    $dados[] = array("nome" => "Maria Antonieta", "idade" => 20, "estilo" => "Rock");
+    $dados[] = array("nome" => "Seu Zé", "idade" => 73, "estilo" => "MPB");
+    $dados[] = array("nome" => "Epaminondas Francisco", "idade" => 13, "estilo" => "Rock");
+    $dados[] = array("nome" => "Eddie", "idade" => 47, "estilo" => "Rock");
+?>
